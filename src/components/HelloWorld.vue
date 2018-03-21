509d56a758eb0f8b75bde57d4208fa625a7935c7
jettFeng <1130034920@>
2018-03-21 23:31:53 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      slider:[
+        {
+          id:14732,
+          linkUrl:"https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=3959774&g_f=shoujijiaodian",
+          picUrl:"http://y.gtimg.cn/music/photo_new/T003R720x288M000002vOkVg0vI14q.jpg"
+        },
+        {
+          id:14767,
+          linkUrl:"https://c.y.qq.com/node/m/client/music_headline/index.html?_hidehd=1&_button=2&zid=656381",
+          picUrl:"http://y.gtimg.cn/music/photo_new/T003R720x288M000001RqiMH2mwk6S.jpg"
+        },
+        {
+          id:14740,
+          linkUrl:"https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=3955742&g_f=shoujijiaodian",
+          picUrl:"http://y.gtimg.cn/music/photo_new/T003R720x288M0000013uxV91zGF6Z.jpg"
+        },{
+          id:14791,
+          linkUrl: "http://y.qq.com/w/album.html?albummid=003eVbQm0WKSfo",
+          picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M000002uzRFP3obs5q.jpg"
+        },{
+          id:14743,
+          linkUrl: "https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=3955752&g_f=shoujijiaodian"
+          picUrl: "http://y.gtimg.cn/music/photo_new/T003R720x288M000003coloK4gPRc1.jpg"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
